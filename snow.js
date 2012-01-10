944deadd8725b01da5d53adeeb3b5e2aa41cf5ba
John McKerrell <john@mckerrell.net>
2012-01-10 15:17:35 +0000

    Reducing quantity of snow.

--- snow.js
+++ snow.js
@@ -8,7 +8,7 @@
   //Configure below to change URL path to the snow image
   var snowsrc="snow.gif"
   // Configure below to change number of snow to render
-  var no = 100;
+  var no = 30;
   // Configure whether snow should disappear after x seconds (0=never):
   var hidesnowtime = 0;
   // Configure how much snow should drop down before fading ("windowheight" or "pageheight")
